2ab361bc33901cbcbaed19dabaf526152625fb37
luiscajl <luisca_jl@hotmail.com>
2017-09-14 20:52:05 +0200

    Adding SpringBoot Pom.xml, application.properties and main start

--- src/main/java/URJC/VideoTranscoding/App.java
+++ src/main/java/URJC/VideoTranscoding/App.java
@@ -1,10 +1,13 @@
 package URJC.VideoTranscoding;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
