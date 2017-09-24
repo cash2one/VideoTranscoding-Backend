a38ab66b2f33972d28f0dabda650ed4b797531b7
luiscajl <luisca_jl@hotmail.com>
2017-09-24 18:33:31 +0200

    Added Travis-CI

---
+++ src/main/java/URJC/VideoTranscoding/controllers/MainController.java
@@ -0,0 +1,14 @@
+package URJC.VideoTranscoding.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+	
+//	@RequestMapping(value="/")
+//	private void index() {
+//		return index;
+//	}
+
+}
