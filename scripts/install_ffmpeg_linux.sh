347d97f7bd476a2489d5df714c5baf5142b640fd
luiscajl <luisca_jl@hotmail.com>
2017-11-14 16:13:10 +0100

    up

--- scripts/install_ffmpeg_linux.sh
+++ scripts/install_ffmpeg_linux.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo add-apt-repository -y ppa:jonathonf/ffmpeg-3
-sudo apt update -y && sudo apt install -y ffmpeg libav-tools x264 x265 vpx vorbis libvorbis theora libogg gpl version3 nonfree postproc libaacplus libass libcelt libfaac libfdk-aac libfreetype libmp3lame libopencore-amrnb libopencore-amrwb libopenjpeg openssl libopus libschroedinger libspeex libtheora libvo-aacenc libvpx libxvid 
-ffmpeg
+sudo apt update -y && sudo apt install ffmpeg  
