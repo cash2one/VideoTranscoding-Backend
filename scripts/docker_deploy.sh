0deff58c4464c0371224e4803cd44d8190756de6
luiscajl <luisca_jl@hotmail.com>
2018-02-28 11:40:21 +0100

    Added DockerDeploy

---
+++ scripts/docker_deploy.sh
@@ -0,0 +1,3 @@
+sudo docker-compose -f /home/luisca/TFG2017_2018/docker-compose.yml stop
+sudo docker-compose -f /home/luisca/TFG2017_2018/docker-compose.yml pull
+sudo docker-compose -f /home/luisca/TFG2017_2018/docker-compose.yml start
