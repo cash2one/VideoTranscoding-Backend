1d81912a5744ed4e7d509c212912ba46bcff959d
luiscajl <luisca_jl@hotmail.com>
2018-03-16 06:22:03 +0100

    JavaDoc

--- src/main/java/es/urjc/videotranscoding/service/ConversionService.java
+++ src/main/java/es/urjc/videotranscoding/service/ConversionService.java
@@ -7,10 +7,33 @@ import es.urjc.videotranscoding.entities.Original;
 import es.urjc.videotranscoding.entities.User;
 
 public interface ConversionService {
-
+	/**
+	 * Save the conversion on the BBDD.
+	 * 
+	 * @param video
+	 *            The video that you will save
+	 */
 	void save(Conversion video);
 
-	User deleteConversion(Original original,Conversion conversion, User u);
+	/**
+	 * Delete a conversion and her original delete it too.
+	 * 
+	 * @param original
+	 *            Video original for delete a conversion.
+	 * @param conversion
+	 *            that you want delete
+	 * @param u
+	 *            User that contain the video. It`s necessary for save the entity
+	 * @return user updated.
+	 */
+	User deleteConversion(Original original, Conversion conversion, User u);
 
+	/**
+	 * Find a conversion and can return the conversion or no.
+	 * 
+	 * @param id
+	 *            for search the conversion
+	 * @return An optional with a conversion
+	 */
 	Optional<Conversion> findOneConversion(long id);
 }
