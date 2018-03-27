093c98700e62fa58a8c4de2255ec6fe7a94cf2d9
luiscajl <luisca_jl@hotmail.com>
2018-03-27 13:04:19 +0200

    Change

--- src/main/java/es/urjc/videotranscoding/restController/UserRestController.java
+++ src/main/java/es/urjc/videotranscoding/restController/UserRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,7 +47,7 @@ public class UserRestController {
 		List<User> users = userService.findAllUsers();
 		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
-
+    @CrossOrigin(origins = "http://localhost:4200/login")
 	@JsonView(Details.class)
 	@ApiOperation(value = "Get one user with full details by id and by name")
 	@RequestMapping(value = "/{id:.*}", method = RequestMethod.GET)
--- src/main/java/es/urjc/videotranscoding/security/SecurityRestConfiguration.java
+++ src/main/java/es/urjc/videotranscoding/security/SecurityRestConfiguration.java
@@ -1,5 +1,15 @@
 package es.urjc.videotranscoding.security;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -11,31 +21,46 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @Order(1)
 @EnableWebSecurity()
-public class SecurityRestConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityRestConfiguration extends WebSecurityConfigurerAdapter implements Filter {
 	@Autowired
 	public UserRepositoryAuthenticationProvider userRepoAuthProvider;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/admin/**").hasRole("ADMIN")
-		.antMatchers("/api/watcher/**").permitAll()
-		.antMatchers("/api/downloader/**").permitAll()
-		.antMatchers("/api/user/register").permitAll()
-		.antMatchers("/api/**").hasRole("USER")//TODO CHANGE THE NEXT LINE FOR THE RELEASE 1.0
-		.antMatchers("/**").hasRole("USER");
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN").antMatchers("/api/watcher/**").permitAll()
+				.antMatchers("/api/downloader/**").permitAll().antMatchers("/api/user/register").permitAll()
+				.antMatchers("/api/**").hasRole("USER")// TODO CHANGE THE NEXT LINE FOR THE RELEASE 1.0
+				.antMatchers("/**").hasRole("USER");
 
 		http.csrf().disable();
-		http.cors().disable();
 		http.httpBasic();
-		// Do not redirect when logout
 		http.logout().logoutSuccessHandler((rq, rs, a) -> {
 		});
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// AQUI HAY JALEEEOOO, DESCOMENTAR
 		auth.authenticationProvider(userRepoAuthProvider);
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, authorization");
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void destroy() {
+		// NOT USED
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// NOT USED
 	}
 }
