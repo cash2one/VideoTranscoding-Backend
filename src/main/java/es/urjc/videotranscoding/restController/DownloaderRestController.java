391d90e4a0f68742f90992ae49be7a15d6ca12e2
luiscajl <luisca_jl@hotmail.com>
2018-04-06 17:52:12 +0200

    Revert changes to download video

--- src/main/java/es/urjc/videotranscoding/restController/DownloaderRestController.java
+++ src/main/java/es/urjc/videotranscoding/restController/DownloaderRestController.java
@@ -1,21 +1,14 @@
 package es.urjc.videotranscoding.restController;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLConnection;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +22,7 @@ import es.urjc.videotranscoding.exception.ExceptionForRest;
 import es.urjc.videotranscoding.exception.FFmpegException;
 import es.urjc.videotranscoding.service.ConversionService;
 import es.urjc.videotranscoding.service.OriginalService;
+import es.urjc.videotranscoding.utils.FileDownloader;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -37,8 +31,6 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "Downloads Api Operations")
 @CrossOrigin(origins = "*")
 public class DownloaderRestController {
-	private static final String TRACE_IO_EXCEPTION_GENERAL = "ffmpeg.ioException.general";
-	protected final Logger logger = Logger.getLogger(DownloaderRestController.class);
 	@Autowired
 	private OriginalService originalService;
 	@Autowired
@@ -62,42 +54,15 @@ public class DownloaderRestController {
 			if (conversion.isPresent()) {
 				Conversion conversionVideo = conversion.get();
 				File filepath = new File(conversionVideo.getPath());
-				String mimeType = URLConnection.guessContentTypeFromName(filepath.getName());
-				if (mimeType == null) {
-					mimeType = "application/octet-stream";
-				}
-				response.setContentType(mimeType);
-				response.setHeader("Content-Disposition",
-						String.format("inline; filename=\"" + filepath.getName() + "\""));
-				response.setHeader("Content-Length", String.valueOf(filepath.length()));
-				try {
-					InputStream inputStream = new BufferedInputStream(new FileInputStream(filepath));
-					FileCopyUtils.copy(inputStream, response.getOutputStream());
-					return new ResponseEntity<>(HttpStatus.OK);
-				} catch (IOException e) {
-					logger.l7dlog(Level.ERROR, TRACE_IO_EXCEPTION_GENERAL, e);
-					throw new FFmpegException(FFmpegException.EX_IO_EXCEPTION_GENERAL);
-				}
+				FileDownloader.fromFile(filepath).with(response).serveResource();
+				return new ResponseEntity<>(HttpStatus.OK);
 			} else {
 				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 			}
 		} else {
 			File filepath = new File(video.get().getPath());
-			String mimeType = URLConnection.guessContentTypeFromName(filepath.getName());
-			if (mimeType == null) {
-				mimeType = "application/octet-stream";
-			}
-			response.setContentType(mimeType);
-			response.setHeader("Content-Disposition", String.format("inline; filename=\"" + filepath.getName() + "\""));
-			response.setHeader("Content-Length", String.valueOf(filepath.length()));
-			try {
-				InputStream inputStream = new BufferedInputStream(new FileInputStream(filepath));
-				FileCopyUtils.copy(inputStream, response.getOutputStream());
-				return new ResponseEntity<>(HttpStatus.OK);
-			} catch (IOException e) {
-				logger.l7dlog(Level.ERROR, TRACE_IO_EXCEPTION_GENERAL, e);
-				throw new FFmpegException(FFmpegException.EX_IO_EXCEPTION_GENERAL);
-			}
+			FileDownloader.fromFile(filepath).with(response).serveResource();
+			return new ResponseEntity<>(HttpStatus.OK);
 		}
 	}
 
