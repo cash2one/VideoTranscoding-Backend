9111d353b0a4ea12c9fddcb2947b547a98380cd2
luiscajl <luisca_jl@hotmail.com>
2018-04-12 02:05:58 +0200

    Patch enabled

--- src/main/java/es/urjc/videotranscoding/security/SecurityRestConfiguration.java
+++ src/main/java/es/urjc/videotranscoding/security/SecurityRestConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityRestConfiguration extends WebSecurityConfigurerAdapter impl
 
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT,PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, authorization, strict-transport-security");
 		chain.doFilter(req, res);
