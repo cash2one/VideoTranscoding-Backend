0fe1e7f29a7495924f68e69b9abac12614b38609
luiscajl <luisca_jl@hotmail.com>
2018-05-23 17:23:37 +0200

    Added test

--- src/test/java/es/urjc/videotranscoding/restController/UserRestControllerTest.java
+++ src/test/java/es/urjc/videotranscoding/restController/UserRestControllerTest.java
@@ -84,4 +84,33 @@ public class UserRestControllerTest {
 		assertEquals(expected.getEmail(), u1.getEmail());
 	}
 
+	@Test
+	public void registerUser() throws Exception {
+		User newUser = new User("email@gmail.com", "newAdmin", "12345678", "", UserRoles.USER);
+		Mockito.when(userService.registerUser(newUser)).thenReturn(newUser);
+		String URL_REGISTER_USER = "/user/register";
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(URL_REGISTER_USER)
+				.accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content("{}");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String outputInJson = result.getResponse().getContentAsString();
+		ObjectMapper mapper2 = new ObjectMapper();
+		User expected = mapper2.readValue(outputInJson, User.class);
+		assertEquals(expected.getNick(), "newAdmin");
+		assertEquals(expected.getEmail(), "email@gmail.com");
+	}
+
+	@Test
+	public void editUser() throws Exception {
+		User newUser = new User("email@gmail.com", "newAdmin", "12345678", "", UserRoles.USER);
+		Mockito.when(userService.registerUser(newUser)).thenReturn(newUser);
+		String URL_REGISTER_USER = "/user/register";
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(URL_REGISTER_USER)
+				.accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content("{}");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String outputInJson = result.getResponse().getContentAsString();
+		ObjectMapper mapper2 = new ObjectMapper();
+		User expected = mapper2.readValue(outputInJson, User.class);
+		assertEquals(expected.getNick(), "newAdmin");
+		assertEquals(expected.getEmail(), "email@gmail.com");
+	}
 }
